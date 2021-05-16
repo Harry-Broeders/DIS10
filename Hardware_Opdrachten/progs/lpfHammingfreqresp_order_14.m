c937af59dcbbce637319e9c77ca88bea6a8d6edf
Harry Broeders <j.z.m.broeders@hr.nl>
2021-05-16 22:47:24 +0200

    Bestanden voor nieuwe versie Hardwareopdracht 2 FIR

---
+++ Hardware_Opdrachten/progs/lpfHammingfreqresp_order_14.m
@@ -0,0 +1,19 @@
+n = [-7:7];
+b = 0.25 * sinc(0.25 * n);
+w = 0.54 + 0.46 * cos(pi/10 * n);
+[H,f] = freqz(b, 1, 512, 8000);
+[Hw,f] = freqz(b.*w, 1, 512, 8000);
+HdB = 20 * log10(abs(H));
+HwdB = 20 * log10(abs(Hw));
+hold on;
+plot(f, HdB, 'Color', [204/255 0 51/255]);
+plot(f, HwdB, 'Color', 'Blue');
+grid;
+ylabel('Magnitude (dB)');
+ylim([-80 5]);
+xlabel('Frequency (Hz)');
+legend('Rectangular window applied', 'Hamming window applied')
+%generate pdf file for use in LaTeX
+file = '../figs/lpfHammingfreqresp';
+print(gcf,'-dpdf', file);
+system(sprintf('pdfcrop %s.pdf %s.pdf', file, file));
---
+++ Hardware_Opdrachten/progs/lpffreqresp_order_14.m
@@ -0,0 +1,12 @@
+b = 0.25 * sinc(0.25 * [-7:7]);
+[H,f] = freqz(b, 1, 512, 8000);
+HdB = 20 * log10(abs(H));
+plot(f, HdB, 'Color', [204/255 0 51/255]);
+grid;
+ylabel('Magnitude (dB)');
+ylim([-45 5]);
+xlabel('Frequency (Hz)');
+%generate pdf file for use in LaTeX
+file = '../figs/lpfvbfreqresp';
+print(gcf,'-dpdf', file);
+system(sprintf('pdfcrop %s.pdf %s.pdf', file, file));
